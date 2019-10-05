7f5cc43f34b76eb843af3f1e5e4127a4ac6b4d20
Renato Avellar Nobre <rekanobre@gmail.com>
2019-10-05 10:01:38 -0300

    Funcao da Main implementada

--- calc.c
+++ calc.c
@@ -1,4 +1,11 @@
-int main() {
+#include <stdio.h>
 
+int main() {
+  float n1, n2;
+  printf("Escreva o primeiro numero: ");
+  scanf("%f", &n1);
+  printf("Escreva o segundo numero: ");
+  scanf("%f", &n2);
+  printf("Os numeros sao %.4f, %.4f\n", n1, n2);
   return 0;
 }
